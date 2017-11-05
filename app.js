35209c747a52c6251da225dcfd7f9a6f5fd896db
ErmineNotyours <arthurallen@centurylink.net>
2017-11-05 05:22:14 -0800

    Prefield works

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 var canvas = document.getElementById('canvas');
 // if (canvas.getContext) {}
 var ctx = canvas.getContext('2d');
-var time = 1000;
-var height = 39;
+var time = 100000; // Rough time delay
+var height = 60;
 var left = 2; // Not followed throughout.  Kept for compatibility
-var width = 70;
+var width = 80;
 var symbols = 3;
 var size = 10; // Pixle size
 // declare multi-dimensional array board, per MDN Indexed Collections
-var rows = 25; // height of prefill board
+var rows = 45; // height of prefill board
 var x1 = 0; // x Location of piece
 var y1 = 0; // y location of piece
 var board = new Array(width + 6);
@@ -31,11 +31,6 @@ for (var i = 0; i < (width + 6); i++){
   }
 }
 
-// Draw borders
-ctx.fillStyle = 'black';
-ctx.fillRect((left * size), 0, size, (size * height));
-ctx.fillRect(((left + width) * size), 0, size, (size * height));
-
 // Pad borders of board
 // bottom row
 for (var fill = 0; fill < (width + 6); fill++){
@@ -46,22 +41,76 @@ for (fill = 0; fill < (height + 3); fill++){
   board[left] [fill] = 15;
   board[left + width + 1] [fill] = 15;
 }
+// Draw borders
+ctx.fillStyle = 'black';
+ctx.fillRect((left * size), 0, size, (size * height));
+ctx.fillRect(((left + width) * size), 0, size, (size * height));
+
 // Here in original version is where you would mark the center bottom of the board for the option of moving on to the next round
 
 // Prefill board
 function prefill(){
-  var guess = 1; // Number of guesses it is taking to find a stable cell
+  //debugger;
+  globalPrefillFlag = false;
   // Blank existing piece for manuall new screen
-  ctx.fillstyle = 'white';
-  ctx.fillRect((x1 * size), (y1 * size), (3 * size), size);
+  ctx.fillStyle = 'white';
+  ctx.fillRect((x1 * size), (y1 * size), size, (3 * size));
   var candidate = 0;
-  var flag = true;
-  // Top of while flag loop (add code)
-  flag = false;
-  // add cycle and check for cycle > 10 code
+  var guess = 1; // Number of guesses it is taking to find a stable cell
+
   for (var i = left + 1; i < width + 2; i++){
     for (var j = height - rows; j < height; j++){
-      candidate = Math.floor(Math.random() * symbols) + 1;
+      var cycle = 0;
+      var flag = true;
+      while (flag) {
+        flag = false;
+        cycle++;
+        console.log('Top of while flag, cycle = ', cycle);
+        if (cycle > 10){
+          guess++;
+          i--; // Yes, I'm decrementing the loop counter
+          console.log('decrementing i: ', i);
+          cycle = 0;
+          if (i < (left + 1)){
+            globalPrefillFlag = true;
+            console.log('i is < 4, returning, ', i);
+            return;
+          }
+        }
+        candidate = Math.floor(Math.random() * symbols) + 1;
+        // Proximity checking
+        // Same symbol horizontal left
+        if (board[i - 1] [j] == candidate){
+          if (board[i - 2] [j] == candidate){
+            flag = true;
+          }
+        }
+        // 610 if array(i - 1, j) = candidate then if array(i - 2, j) = candidate then flag = 1 : rem same symbol horz left
+
+        // Same symbol up
+        if (board[i] [j - 1] == candidate){
+          if (board[i] [j - 2] == candidate){
+            flag = true;
+          }
+        }
+        // 620 if array(i, j - 1) = candidate then if array(i, j - 2) = candidate then flag = 1 : rem same symbol up
+        // Same symbol diagonally left and up
+        if (board[i - 1] [j - 1] == candidate){
+          if (board[i - 2] [j - 2] == candidate){
+            flag = true;
+          }
+        }
+        // 630 if array(i - 1, j - 1) = candidate then if array(i - 2, j - 2) = candidate then flag = 1 : rem same symbol diag left & up
+        // Same symbol diagonally left and down
+        if (board[i - 1] [j + 1] == candidate){
+          if (board [i - 2] [j + 2] == candidate){
+            flag = true;
+          }
+        }
+        // 640 if array(i - 1, j + 1) = candidate then if array(i - 2, j + 2) = candidate then flag = 1 : rem same symbol diag left & down
+        console.log('At end of while flag loop: flag=', flag);
+      } // wend flag
+      // Now that we have a viable candidate:
       switch (candidate){
       case 1:
         ctx.fillStyle = 'red';
@@ -79,7 +128,13 @@ function prefill(){
       // ctx.fillRect(0, 0, 50, 50);
       // ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
       // ctx.fillRect(30, 30, 50, 50);
+
     } // Next j
   } // Next i
 }
-prefill();
+var globalPrefillFlag = true;
+// It's possible that the function prefill will be so unable to find a stable board with no three in a row that it will have to start over again.  globalPrefillFlag helps do this.
+while (globalPrefillFlag){
+  globalPrefillFlag = false;
+  prefill();
+}
